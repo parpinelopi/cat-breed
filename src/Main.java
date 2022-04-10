46305dd884ab0a774cd3369fd0f5a2bf7062b05a
parpinelopi <pennyblack.p@gmail.com>
2022-04-10 20:47:41 +0200

    create buffer reader in main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,52 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-
-    List<Cat> cats = new ArrayList<>();
+    private static Gender stringToGender(String catGender) {
+        Gender gender;
+        switch (catGender) {
+            case "female":
+                gender = Gender.FEMALE;
+                break;
+            default:
+                gender = Gender.MALE;
+        }
+        return gender;
+    }
 
     public static void main(String[] args) {
 
+        List<Cat> cats = new ArrayList<>();
+
+
+        try {
+            BufferedReader readFromFile = new BufferedReader(new FileReader("cats.csv"));
+            readFromFile.readLine();
+
+            while (true) {
+
+                String value = readFromFile.readLine();
+                if (value == null) {
+                    break;
+                } else {
+                    String[] values = value.split(",");
+                    Cat cat = new Cat(values[0], values[1], values[2], stringToGender(values[3]));
+                    cats.add(cat);
+                }
+            }
+
+            for (Cat cat : cats) System.out.println(cat);
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
 
 
     }
 }
+
+
