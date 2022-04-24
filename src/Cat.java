d4a59c562405a1fc962a1504d0c0c150cb131f0f
parpinelopi <pennyblack.p@gmail.com>
2022-04-24 18:11:32 +0200

    update method calculateAverageAge

--- src/Cat.java
+++ src/Cat.java
@@ -1,21 +1,27 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
 public class Cat {
 
     private String name;
     private String breed;
-    private String dateOfBirth;
+    private LocalDate birthdate;
     private Gender gender;
 
+    // ArrayList<int> catAges = new ArrayList<int>();
 
-    public Cat(String name, String breed, String dateOfBirth, Gender gender) {
+
+    public Cat(String name, String breed, LocalDate birthdate, Gender gender) {
         this.name = name;
         this.breed = breed;
-        this.dateOfBirth = dateOfBirth;
+        this.birthdate = birthdate;
         this.gender = gender;
     }
 
     @Override
     public String toString() {
-        return "name: " + name + " breed: " + breed + " dateOfBirth: " + dateOfBirth + " gender: " + gender;
+        return "name: " + name + " breed: " + breed + " dateOfBirth: " + birthdate + " gender: " + gender;
     }
 
     public String getName() {
@@ -26,13 +32,39 @@ public class Cat {
         return breed;
     }
 
-    public String getDateOfBirth() {
-        return dateOfBirth;
+    public LocalDate getDateOfBirth() {
+        return birthdate;
     }
 
     public Gender getGender() {
         return gender;
     }
 
+    public int ageInt() {
+        LocalDate currentDate = LocalDate.now();
+        int catAge;
+
+
+        if ((birthdate != null) && (currentDate != null)) {
+            catAge = Period.between(birthdate, currentDate).getYears();
+            return catAge;
+        } else {
+            return 0;
+        }
+    }
+
+
+    public static int calculateAverageAge( List<Cat> cats) {
+        int ageSum = 0;
+        int avgAge = 0;
+        for (Cat cat : cats) {
+
+           ageSum += cat.ageInt();
+
+        }
+        avgAge = ageSum/cats.size();
+        return avgAge;
+
+    }
 
 }
