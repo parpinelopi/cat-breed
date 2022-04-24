be38c109fa49c5aab31239fd7fdcccf4741f17f7
parpinelopi <pennyblack.p@gmail.com>
2022-04-24 18:10:24 +0200

    change to LocalDate class for calculating the average

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
 
@@ -37,22 +37,25 @@ public class Main {
                 if (value == null) {
                     break;
                 } else {
+
                     String[] values = value.split(",");
                     dateOfBirth = values[2];
-                    SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-                    Date date = formatter.parse(dateOfBirth);
-                    System.out.println(date);
-                    Cat cat = new Cat(values[0], values[1], date, stringToGender(values[3]));
 
 
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-MM-yyyy", Locale.US);
+                    LocalDate formatDateTime = LocalDate.parse(dateOfBirth, formatter);
+
+                    System.out.println(formatDateTime);
+                    Cat cat = new Cat(values[0], values[1], formatDateTime, stringToGender(values[3]));
                     cats.add(cat);
 
                 }
             }
 
             for (Cat cat : cats) System.out.println(cat);
+            System.out.println(Cat.calculateAverageAge(cats));
 
-        } catch (IOException | ParseException ioException) {
+        } catch (IOException ioException) {
             ioException.printStackTrace();
         }
 
